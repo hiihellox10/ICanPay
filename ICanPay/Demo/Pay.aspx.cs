33b5a5b5d3eed188019e8b54657075348bb6a12d
dotshopfield <dotshopfield@01c3915b-a14f-0410-aa0e-a15d854ec95e>
2008-07-12 10:05:33 +0000

    将输出订单的接口调用统一为Payment，而不是以前那样调用BuildPaymentUrl或者BuildPaymentForm

--- ICanPay/Demo/Pay.aspx.cs
+++ ICanPay/Demo/Pay.aspx.cs
@@ -21,8 +21,8 @@ namespace Demo
             PaymentSetting<YeepayGateway> ps = new PaymentSetting<YeepayGateway>();
             ps.PayGateway.Merchant.UserName = "10000432521";
             ps.PayGateway.Merchant.Key = "8UPp0KE8sq73zVP370vko7C39403rtK1YwX40Td6irH216036H27Eb12792t";
-            ps.PayGateway.Merchant.NotifyUrl = "http://yousite.com/Notify.aspx";
-            ps.PayGateway.Order.OrderId = "1564515";
+            ps.PayGateway.Merchant.NotifyUrl = "http://yousite.com/Pay/notify.aspx";
+            ps.PayGateway.Order.OrderId = "1411515";
             ps.PayGateway.Order.Amount = 0.01;
             ps.PayGateway.Customer.Name = "name";
             ps.PayGateway.Customer.Email = "name@address.com";
@@ -30,8 +30,8 @@ namespace Demo
             ps.PayGateway.Customer.Post = "4564565";
             ps.PayGateway.Customer.Telephone = "1568847888";
 
-            // 创建付款的Url
-            Response.Redirect(ps.PayGateway.BuildPaymentUrl());
+            // 转到支付网站付款
+            ps.Payment();
         }
     }
 }
